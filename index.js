38cce8c4df9e53f222808b3142229af1063fb394
aakash97 <akash.negi@nirmitee.io>
2022-06-19 02:14:25 +0530

    update data with axios

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
+const { default: axios } = require("axios");
 const puppeteer = require("puppeteer");
 
+// "https://dynamic-portfolio-api.herokuapp.com/ " + "portfolio/getBlogs",
+const getblogList = async () => {
+  let data = await axios.get(
+    "https://dynamic-portfolio-api.herokuapp.com/portfolio/getBlogs"
+  );
+  // console.log(data.data);
+  return data.data;
+};
+
 (async () => {
+  let data;
+  try {
+    data = await getblogList();
+  } catch (err) {
+    console.log(err.message);
+  }
   const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
   const page = await browser.newPage();
   await page.setViewport({ width: 1366, height: 768 });
 
-  await page.goto(
-    "https://primeprogrammingworld.blogspot.com/2021/06/leetcode-max-area-of-island-solution.html"
-  );
-  //   await page.screenshot({ path: "example.png" });
-  await autoScroll(page);
-
+  // console.log(data);
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
+    await page.goto(data[i]?.link);
+    //   await page.screenshot({ path: "example.png" });
+    await autoScroll(page);
+  }
+  // data?.map(async (ele) => {
+  //   console.log(ele)
+  //   await page.goto(ele?.link);
+  //   //   await page.screenshot({ path: "example.png" });
+  //   await autoScroll(page);
+  //   // await browser.close();
+  // });
   // await page.goto(
   //   "https://primeprogrammingworld.blogspot.com/2021/05/problem-158a-solution-next-round.html"
   // );
 
   // await autoScroll(page);
 
-  setTimeout(async () => {
+  // setTimeout(async () => {
     await browser.close();
-  }, 10000);
+  // }, 500);
 })();
 
 async function autoScroll(page) {
@@ -27,11 +51,11 @@ async function autoScroll(page) {
     await new Promise((resolve, reject) => {
       // for (let i = 0; i <= 10; i++) {
       var totalHeight = 0;
-      var distance = Math.floor(Math.random() * 25 + 1);
+      var distance = Math.floor(Math.random() * 15 + 1);
       var timer = setInterval(() => {
-        distance = Math.floor(Math.random() * 25 + 1)
+        distance = Math.floor(Math.random() * 55 + 1);
         var scrollHeight = document.body.scrollHeight;
-        console.log(distance)
+        console.log(distance);
         window.scrollBy(0, distance);
         totalHeight += distance;
 
@@ -39,9 +63,9 @@ async function autoScroll(page) {
           clearInterval(timer);
           resolve();
         }
-      }, Math.floor(Math.random() * 2000 + 1));
+      }, Math.floor(Math.random() * 1000 + 1));
       // }
     });
   });
-  return height;
+  return 
 }
